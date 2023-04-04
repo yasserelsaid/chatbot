1b5b0402f3a702c0be960ffd3da10c533000d071
Yasser Elsaid <yassermelsaeed@gmail.com>
2023-04-04 01:51:52 -0400

    x

--- index.js
+++ index.js
@@ -2,18 +2,7 @@ const CHAT_BUTTON_SIZE = 50 // size of the chat button in pixels
 const CHAT_BUTTON_RADIUS = CHAT_BUTTON_SIZE / 2 // radius of the chat button in pixels
 const CHAT_BUTTON_BACKGROUND_COLOR = 'black' // background color of the chat button
 const scriptTag = document.currentScript
-const CHAT_BUTTON_ICON = `
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="#FFFFFF" width="24" height="24">
-<path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 011.037-.443 48.282 48.282 0 005.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
-</svg>
-
-`
-
-const CHAT_BUTTON_CLOSE_ICON = `
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="#FFFFFF" width="24" height="24">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-</svg>
-`
+let ICON_COLOR = 'white'
 
 // create the chat button element
 const chatButton = document.createElement('div')
@@ -50,7 +39,6 @@ chatButtonIcon.style.height = '100%'
 chatButtonIcon.style.zIndex = 999999999
 
 // add the chat button icon to the chat button element
-chatButtonIcon.innerHTML = CHAT_BUTTON_ICON
 
 chatButton.appendChild(chatButtonIcon)
 
@@ -61,10 +49,10 @@ chatButton.addEventListener('click', () => {
   // toggle the chat component
   if (chat.style.display === 'none') {
     chat.style.display = 'flex'
-    chatButtonIcon.innerHTML = CHAT_BUTTON_CLOSE_ICON
+    chatButtonIcon.innerHTML = getChatButtonCloseIcon()
   } else {
     chat.style.display = 'none'
-    chatButtonIcon.innerHTML = CHAT_BUTTON_ICON
+    chatButtonIcon.innerHTML = getChatButtonIcon()
   }
 })
 
@@ -123,8 +111,52 @@ const getChatButtonColor = async () => {
   )
 
   const data = await response.json()
+
   chatButton.style.backgroundColor = data.color || CHAT_BUTTON_BACKGROUND_COLOR
   document.body.appendChild(chatButton)
+
+  const iconColor = getContrastingTextColor(
+    data.color || CHAT_BUTTON_BACKGROUND_COLOR
+  )
+
+  console.log('iconColor', iconColor)
+  ICON_COLOR = iconColor
+  chatButtonIcon.innerHTML = getChatButtonIcon()
+}
+
+function getChatButtonIcon() {
+  const CHAT_BUTTON_ICON = `
+  <svg id="chatIcon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.3" stroke="${ICON_COLOR}" width="24" height="24">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 011.037-.443 48.282 48.282 0 005.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
+  </svg>`
+  return CHAT_BUTTON_ICON
+}
+
+function getChatButtonCloseIcon() {
+  const CHAT_BUTTON_CLOSE_ICON = `
+  <svg id="closeIcon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.3" stroke="${ICON_COLOR}" width="24" height="24">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+  </svg>
+  `
+  return CHAT_BUTTON_CLOSE_ICON
+}
+
+function getContrastingTextColor(bgColor) {
+  // Ensure the input is in the format #RRGGBB
+  if (bgColor.charAt(0) === '#') {
+    bgColor = bgColor.substr(1)
+  }
+
+  // Convert the input color to RGB
+  const r = parseInt(bgColor.substr(0, 2), 16)
+  const g = parseInt(bgColor.substr(2, 2), 16)
+  const b = parseInt(bgColor.substr(4, 2), 16)
+
+  // Calculate the luminance value using the WCAG formula
+  const luminance = (0.299 * r + 0.587 * g + 0.114 * b) / 255
+
+  // Return the appropriate text color based on the luminance value
+  return luminance > 0.5 ? 'black' : 'white'
 }
 
 getChatButtonColor()
